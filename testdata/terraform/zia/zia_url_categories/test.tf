3a9f44b7a9412af720fa40b079b4242e2df6b637
GitHub Terraform <zscaler-github-P@zscaler.com>
2022-10-17 00:49:35 -0700

    Fix test

--- testdata/terraform/zia/zia_url_categories/test.tf
+++ testdata/terraform/zia/zia_url_categories/test.tf
@@ -1,7 +1,11 @@
 resource "zia_url_categories" "terraform_managed_resource" {
-  custom_category                      = false
-  description                          = "OTHER_ADULT_MATERIAL_DESC"
+  configured_name                      = "MS Defender Unsanctioned Apps"
+  custom_category                      = true
+  custom_urls_count                    = 33
+  db_categorized_urls                  = [".creditkarma.com", ".youku.com"]
+  description                          = "MCAS Unsanctioned Apps"
   editable                             = true
   type                                 = "URL_CATEGORY"
-  urls_retaining_parent_category_count = 0
+  urls                                 = [".Logz.io",".aa.com",".accuweather.com",".agoda.com"]
+  urls_retaining_parent_category_count = 2
 }
